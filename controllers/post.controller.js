ed29e6680aa4ea99a013f3e5c670676fb40db564
rasangalk <rasangalakshith38@gmail.com>
2021-09-05 20:04:51 +0530

    inster posts to DB

---
+++ controllers/post.controller.js
@@ -0,0 +1,27 @@
+const models = require("../models");
+
+function save(req, res) {
+	const post = {
+		title: req.body.title,
+		content: req.body.content,
+		imageUrl: req.body.image_url,
+		categoryId: req.body.category_id,
+		userId: 1,
+	};
+
+	models.Post.create(post)
+		.then(result => {
+			res.status(200).json({
+				message: "Post created successfully",
+				post: result,
+			});
+		})
+		.catch(error => {
+			res.status(200).json({
+				message: "Something went wrong",
+				error: error,
+			});
+		});
+}
+
+module.exports = { save: save };
---
+++ routes/posts.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const postsController = require("../controllers/post.controller");
+
+const router = express.Router();
+
+router.post("/", postsController.save);
+
+module.exports = router;
+
